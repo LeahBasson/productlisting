6083458a7b59b67b03d331aaa94ce4bbdd71530f
LeahBasson <leahbasson54@gmail.com>
2024-06-25 10:29:46 +0200

    initial commit

---
+++ src/components/Card.vue
@@ -0,0 +1,34 @@
+<template>
+
+ <div class="card">
+  <div class="card-header">
+
+    <slot name="cardHeader"></slot>
+
+  </div>
+  <div class="card-body">
+    
+    <slot name="cardBody"></slot>
+
+  </div>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.card{
+  width: 18rem;
+}
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,31 @@
 import { createStore } from 'vuex'
+const productURL = 'http://localhost:3000/result'
 
 export default createStore({
   state: {
+    products: null,
+    product : null
   },
   getters: {
   },
   mutations: {
+    setProducts(state, products) {
+      state.products = products
+    },
+    setProduct(state, product) {
+      state.product = product
+    }
   },
   actions: {
+    async fetchProducts(context) {
+      try{
+        let res = await fetch(productURL)
+        let data = await res.json()
+        context.commit('setProducts', data)
+      } catch (e) {
+        console.log(e.message);
+      }
+    }
   },
   modules: {
   }
